5beba65a1d0d273f8638a4d5d769a783ff2488c2
Bruno Gomide <brunoggomide@gmail.com>
2021-10-25 13:23:15 -0300

    parcial

---
+++ crud/home.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include('autenticacao.php');    
+?>
+
+<head>
+<link rel="stylesheet" href="css/home.css">
+</head>
+
+<h1>Olá, <?php echo $_SESSION['user'];?>!</h1>
+<br>
+<br>
+<br>
+<h3>Apresentação da avaliação parcial de web - 2!</h3>
+<br>
+<br>
+<br>
+<div class="alunos"> 
+    <p>Ana Laura Alves</p>
+    <p>Bruno Gabriel Gomide</p>
+    <p>Nelson José Dias Mello</p>
+    <p>Werner</p>
+</div>
